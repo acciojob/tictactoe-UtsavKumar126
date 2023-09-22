dc96cf76ac7495748ea50e9b643efa4b58621fb7
UtsavKumar126 <117590564+UtsavKumar126@users.noreply.github.com>
2023-09-22 14:51:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -26,21 +26,26 @@ for(let i=0;i<box.length;i++){
 function mark(event) {
 	if(turn==true){
 		event.target.textContent="x";
-		if(checkWin())message.textContent=p1+",congratulations you won!";
+		if(checkWin(true)){
+			message.textContent=p1+",congratulations you won!";
+			return;
+		}
 		message.textContent=p2+", you're up";
 		turn=false;
 		//event.removeEventListener("click",(event)=>{});
 	}
 	else{
 		event.target.textContent="o";
-		if(checkWin())message.textContent=p1+",congratulations you won!";
+		if(checkWin(false)){
+			message.textContent=p2+",congratulations you won!";
+			return;
+		}
 		message.textContent=p1+", you're up";
 		turn=true;
 		//event.removeEventListener("click",(event)=>{});
 	}
 }
-function checkWin() {
-	const div1=document.getElementById("1")
+    const div1=document.getElementById("1")
 	const div2=document.getElementById("2")
 	const div3=document.getElementById("3")
 	const div4=document.getElementById("4")
@@ -48,5 +53,23 @@ function checkWin() {
 	const div6=document.getElementById("6")
 	const div7=document.getElementById("7")
 	const div8=document.getElementById("8")
-	const div9=document.getElementById("9")	
+	const div9=document.getElementById("9")
+function checkWin(player) {
+	if(player){
+		if(div1.textContent==='x'&&div2.textContent==='x'&&div3.textContent==='x')return true;
+		else if(div1.textContent=='x'&&div4.textContent=='x'&&div7.textContent=='x')return true;
+		else if(div3.textContent=='x'&&div6.textContent=='x'&&div9.textContent=='x')return true;
+		else if(div7.textContent=='x'&&div8.textContent=='x'&&div9.textContent=='x')return true;
+		else if(div1.textContent=='x'&&div5.textContent=='x'&&div9.textContent=='x')return true;
+		else if(div3.textContent=='x'&&div5.textContent=='x'&&div7.textContent=='x')return true;
+	}
+	else{
+		if(div1.textContent=='o'&&div2.textContent=='o'&&div3.textContent=='o')return true;
+		else if(div1.textContent=='o'&&div4.textContent=='o'&&div7.textContent=='o')return true;
+		else if(div3.textContent=='o'&&div6.textContent=='o'&&div9.textContent=='o')return true;
+		else if(div7.textContent=='o'&&div8.textContent=='o'&&div9.textContent=='o')return true;
+		else if(div1.textContent=='o'&&div5.textContent=='o'&&div9.textContent=='o')return true;
+		else if(div3.textContent=='o'&&div5.textContent=='o'&&div7.textContent=='o')return true;
+	}
+	return false;
 }
